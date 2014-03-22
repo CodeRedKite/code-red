41dae94189ca473cd35e7911df8efb654ec0bf18
Ben <benbruscella@gmail.com>
2014-03-22 16:37:15 +1100

    new api

--- argo/assets/js/red.js
+++ argo/assets/js/red.js
@@ -1,14 +1,25 @@
+// Returns a random integer between min and max
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 jQuery(function($) {
-  $('#submission_form_button').click( function() {
-    alert('shared!');
-      // $.ajax({
-      //     url: 'some-url',
-      //     type: 'post',
-      //     dataType: 'json',
-      //     data: $('form#myForm').serialize(),
-      //     success: function(data) {
-      //                ... do something with the data...
-      //              }
-      // });
+  $('.brick3').hide();
+  $.get( "http://red.pccv.org.au/profiles/form2", function( data ) {
+    json = JSON.parse(data);
+    console.log(json);
+    question = json.data[getRandomInt(0, json.data.length - 1)]
+    console.log(question);
+    console.log(question.text);
+    $('.brick3 .inner h1').html(question.text);
+    $('.brick3 .inner p').html("");
+    // console.log(question.answers);
+    // console.log(question.answers.length);
+    // for (var i = 0; i < question.answers.length; i++) {
+    //   console.log(question.answers[i]);
+    //   $('.brick3 .inner p').append(question.answers[i].title);
+    // }
+    $('.brick3').show('slow');
   });
 });
