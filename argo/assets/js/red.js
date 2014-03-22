e03782ee687a09a8c2643365b15c57313c7587d3
Ben <benbruscella@gmail.com>
2014-03-22 12:50:17 +1100

    share hooks

---
+++ argo/assets/js/red.js
@@ -0,0 +1,14 @@
+jQuery(function($) {
+  $('#submission_form_button').click( function() {
+    alert('shared!');
+      // $.ajax({
+      //     url: 'some-url',
+      //     type: 'post',
+      //     dataType: 'json',
+      //     data: $('form#myForm').serialize(),
+      //     success: function(data) {
+      //                ... do something with the data...
+      //              }
+      // });
+  });
+});
