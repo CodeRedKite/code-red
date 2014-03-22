97a6aafbffd6991b49b65ee33b811fcff39ed57e
Ben <benbruscella@gmail.com>
2014-03-22 19:20:17 +1100

    infographic

--- argo/assets/js/red.js
+++ argo/assets/js/red.js
@@ -9,6 +9,7 @@ jQuery(function($) {
   $('.brick3').hide();
   $('.brick3 .inner p').hide();
   $('.brick3 .inner input').hide();
+  $("#infographic").hide();
   $.get( "http://red.pccv.org.au/profiles/form2", function( data ) {
     json = JSON.parse(data);
     console.log(json);
@@ -21,13 +22,16 @@ jQuery(function($) {
       console.log(question.answers[i]);
       $('.brick3 .inner #options').append(question.answers[i]["title"] + ' <input type="radio" name="radio" value="value1"></input><br/>');
     }
+    $('.brick3 .inner img').attr('src', 'http://red.pccv.org.au/img/' + question.image);
     $('.brick3').show();
   });
   
   $("#options").on('click', function(e) {
     e.preventDefault();
+    $(".front").hide();
     $('.brick3 .inner p').show();
-      $('.brick3 .inner input').show();
+    $('.brick3 .inner input').show();
+    $("#infographic").show();
   });
 
     $('.next').click(function() {
