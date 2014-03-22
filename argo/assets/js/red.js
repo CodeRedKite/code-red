d63360144ee163e1c55e858139d8bd27ee0f1f8c
Chris Ward <chriswhward@gmail.com>
2014-03-22 14:42:28 +1100

    Simpler refactoring done

--- argo/assets/js/red.js
+++ argo/assets/js/red.js
@@ -1,14 +1,29 @@
 jQuery(function($) {
-  $('#submission_form_button').click( function() {
-    alert('shared!');
-      // $.ajax({
-      //     url: 'some-url',
-      //     type: 'post',
-      //     dataType: 'json',
-      //     data: $('form#myForm').serialize(),
-      //     success: function(data) {
-      //                ... do something with the data...
-      //              }
-      // });
-  });
+    $('.next').click(function() {
+        $('#answer').slideToggle();
+        $('#navbar').slideToggle();
+
+        $('.step1').slideToggle();
+    });
+
+    $('#btn_up').click(function() {
+        $('#answer').slideToggle();
+        $('#navbar').slideToggle();
+
+        $('.step1').slideToggle();
+    });
+
+
+    $('#submission_form_button').click(function() {
+        alert('shared!');
+        // $.ajax({
+        //     url: 'some-url',
+        //     type: 'post',
+        //     dataType: 'json',
+        //     data: $('form#myForm').serialize(),
+        //     success: function(data) {
+        //                ... do something with the data...
+        //              }
+        // });
+    });
 });
