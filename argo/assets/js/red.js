f68ded24c0299456b9cdad9da10f0def40b5e48b
Ben <benbruscella@gmail.com>
2014-03-23 14:59:06 +1100

    Revert "Revert "responsive""
    This reverts commit d3c92d860714458476d69714a3785429b63b4c2e.

--- argo/assets/js/red.js
+++ argo/assets/js/red.js
@@ -5,12 +5,12 @@ function getRandomInt(min, max) {
 }
 
 jQuery(function($) {
-    $('#submission_form_button').hide();
+    $('.submission_form_button').hide();
     $('.brick3').hide();
     $('.share_text').hide();
     $('.brick3 .inner p').hide();
     $('.brick3 .inner input').hide();
-    $("#infographic").hide();
+    $(".infographic").hide();
 
     $.get("http://red.pccv.org.au/profiles/question", function(data) {
         json = JSON.parse(data);
@@ -25,7 +25,7 @@ jQuery(function($) {
             $('.brick3 .inner .options').append('<div class="radio-set"><span class="radio-button"></span><span class="radio-text ' + question.answers[i]["correct"] + '">' + question.answers[i]["title"] + '</span>' + '</div>');
         }
 
-        $('.brick3 .inner #infographic').attr('src', 'assets/img/' + question.image);
+        $('.brick3 .inner .infographic').attr('src', 'assets/img/' + question.image);
         $('.brick3').show();
 
     });
@@ -35,8 +35,8 @@ jQuery(function($) {
         $(".front").hide();
         $('.brick3 .inner p').show();
         $('.brick3 .inner input').show();
-        $("#infographic").show();
-        $('#submission_form_button').show();
+        $(".infographic").show();
+        $('.submission_form_button').show();
         $('.share_text').show();
 
         $('.brick3 .inner h1').html("RedKite lightens the load with…");
@@ -56,7 +56,7 @@ jQuery(function($) {
         $('.step1').slideToggle();
     });
 
-    $('#submission_form_button').click(function() {
+    $('.submission_form_button').click(function() {
         //alert('shared!');
         $.ajax({
             url: 'http://red.pccv.org.au/profiles/share',
@@ -65,14 +65,14 @@ jQuery(function($) {
             data: $('form#myForm').serialize(),
             success: function(data) {
                 $('.brick3 .inner h1').html("Thank you for helping redkite lighten the load of familes all around Australia.<p class='find-more'>Find out more…");
-                $('.brick3 .inner #infographic').hide();
+                $('.brick3 .inner .infographic').hide();
 
             }
         });
     });
 
-    $('#submission_form_button').click(function() {
-        $('#submission_form_button').attr({
+    $('.submission_form_button').click(function() {
+        $('.submission_form_button').attr({
             src: 'assets/img/share_but_on.png'
         });
     });
